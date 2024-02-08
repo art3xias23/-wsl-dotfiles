a5d497ee851cd6a1491bbf047b7d02b7b50097be
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-02-08 23:45:55 +0000

    Removed ftplugin folder, configured lsp_zero and autocompletion snippets, added omnisharp and lua_ls

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,86 +1,42 @@
-local on_attach = function(_, bufnr)
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts)
-	vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.keymap.set("n", "gh", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.keymap.set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	vim.keymap.set("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	vim.keymap.set("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	vim.keymap.set("n", "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-	vim.keymap.set("n", "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	vim.keymap.set("n", "<leader>dl", "<cmd>lua vim.lsp.diagnostic.setloclist()<CR>", opts)
-end
+local lsp_zero = require("lsp-zero")
+lsp_zero.on_attach(function(_, bufnr)
+	lsp_zero.default_keymaps({buffer = bufnr})
+end)
 
-local rounded_border_handlers = {
-	["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-	["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-}
+local lua_opts = lsp_zero.nvim_lua_ls()
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
-require("mason").setup()
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
+})
+
+
+require('lspconfig').lua_ls.setup(lua_opts)
+
+require("mason").setup({})
 
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"lua_ls",
 		"omnisharp",
 	},
-})
-require("mason-lspconfig").setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			handlers = rounded_border_handlers,
-			completion = {
-			completeUnimported = true,
-		triggerCharacters = {".", ":"}}
-		})
-	end,
-	["omnisharp"] = function()
-		require("lspconfig")["omnisharp"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			root_dir = function(fname)
-				local lspconfig = require("lspconfig")
-				local primary = lspconfig.util.root_pattern("*.sln")(fname)
-				local fallback = lspconfig.util.root_pattern("*.csproj")(fname)
-				return primary or fallback
-			end,
-			analyze_open_documents_only = true,
-			organize_imports_on_format = true,
-			handlers = vim.tbl_extend("force", rounded_border_handlers, {
-				["textDocument/definition"] = require("omnisharp_extended").handler,
-			}),	completion = {
-			completeUnimported = true,
-		triggerCharacters = {".", ":"}}
-		})
-	end,
-	["lua_ls"] = function()
-
-		require("lspconfig")["lua_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-
-			handlers =rounded_border_handlers,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = {"vim"}
-					}
-
-				}
-
-			}
-		})
-	end
+	handlers = {
+		lsp_zero.default_setup,
+	}
 })

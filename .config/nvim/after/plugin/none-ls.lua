ced9e5110a01dc41e01c97fb1144839e233b456d
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-02-11 22:01:49 +0000

    Fixed up none-ls

--- .config/nvim/after/plugin/none-ls.lua
+++ .config/nvim/after/plugin/none-ls.lua
@@ -1,15 +1,7 @@
-print("none-ls")
-return{
-"nvimtools/none-ls.nvim",
-
-config = function()
-
-local null_ls = require('null-ls')
+local null_ls = require("null-ls")
 
 null_ls.setup({
-sources = {
-null_ls.builtins.formatting.stylua,
-}})
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-end
-}
+	sources = {
+		null_ls.builtins.formatting.stylua,
+	},
+})

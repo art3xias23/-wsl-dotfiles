041d11f9a225bdfdfc4a90fe76cdd75ce319ccd9
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-02-17 21:21:18 +0000

    Installed dressing plugin

--- .config/nvim/lua/art3xias/plugins.lua
+++ .config/nvim/lua/art3xias/plugins.lua
@@ -89,8 +89,11 @@ return {
 	},
 
 	--open buffers in tabs and style them
-	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 
 	--autoclose html tags
-	{"windwp/nvim-ts-autotag", lazy = true}
+	{ "windwp/nvim-ts-autotag", lazy = true },
+	{
+		"stevearc/dressing.nvim"
+	},
 }

3154e4e30ceb9e2fc843926cafc9736bdad03290
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-02-17 21:36:56 +0000

    Added flash for labels on searches

--- .config/nvim/lua/art3xias/plugins.lua
+++ .config/nvim/lua/art3xias/plugins.lua
@@ -93,7 +93,20 @@ return {
 
 	--autoclose html tags
 	{ "windwp/nvim-ts-autotag", lazy = true },
+
+	-- search highlight and additional helpers
 	{
-		"stevearc/dressing.nvim"
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		},
 	},
 }

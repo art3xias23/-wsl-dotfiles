06b0757a5b41417f6e31690ec086f79df8856dee
Konstantin Milchev <konstantin.v.milchev@gmail.com>
2024-02-22 23:55:51 +0000

    Toying with dap configuration

--- .config/nvim/after/plugin/dap-adapters.lua
+++ .config/nvim/after/plugin/dap-adapters.lua
@@ -1,4 +1,5 @@
 local dap = require("dap")
+dap.set_log_level('TRACE')
 dap.adapters.coreclr = {
   type = 'executable',
   command = '/usr/local/netcoredbg',
--- .config/nvim/after/plugin/dap-configuration.lua
+++ .config/nvim/after/plugin/dap-configuration.lua
@@ -5,7 +5,19 @@ dap.configurations.cs = {
     name = "launch - netcoredbg",
     request = "launch",
     program = function()
-        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
     end,
+      justMyCode = false,
+    stopAtEntry = false,
+    env = {
+      ASPNETCORE_ENVIRONMENT = function()
+        -- todo: request input from ui
+        return "Development"
+      end,
+      ASPNETCORE_URLS = function()
+        -- todo: request input from ui
+        return "http://localhost:8080"
+      end,
+    },
   },
 }
